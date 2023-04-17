45d3b54994a6d40f0bf0930e68909c8412f075b3
KennySabee <kemerizalde.dwfsc10@ucamp.utel.edu.mx>
2023-04-17 10:28:01 -0500

    inicio proyecto

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,38 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <title>Club Chino YV | Escuela de Chino Mandarin</title>
+
+        <meta
+          name="description"
+          content="El club ChinoYV ofrece cursos de chino mandarin presencial o en línea son impartidos por profesores altamente capacitados y adaptados a tu nivel y necesidades. Aprende mandarín de forma fácil y efectiva con nuestra plataforma de aprendizaje o en nuestras aulas ubicadas en Quito, valle de los chillos  "
+        />
+        <meta
+          name="keywords"
+          content="Aprender mandarín, Idioma chino, Clases de chino, Estudiar chino, Cultura china, HSK (Examen de chino como lengua extranjera), Traducción chino, Aprender chino para viajar, Clases de mandarín en línea, Palabras básicas en chino, Aprender chino rápido."
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={"true"}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
